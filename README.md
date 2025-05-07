8ba747244bb5fd45bb6dcc29e0c5933e3aa27ded
Truman Mulholland <truman.mulholland@gmail.com>
2025-05-07 17:19:56 +1200

    Installable from repo install script

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dotfiles
+## Install
+```pwsh
+pwsh -ExecutionPolicy Bypass -c "irm https://raw.githubusercontent.com/trumully/dotfiles/refs/heads/main/install.ps1 | iex"
+```
--- install.ps1
+++ install.ps1
@@ -1,6 +1,3 @@
-Set-StrictMode -Version Latest
-
-# Install WinGet
 $hasWinget = Get-Command winget -ErrorAction SilentlyContinue
 if (-not $hasWinget) {
     Write-Host "Installing WinGet" 
@@ -9,23 +6,10 @@ if (-not $hasWinget) {
     del winget.msixbundle
 }
 
-$hasPs7 = Get-Command pwsh -ErrorAction SilentlyContinue
-if (-not $hasPs7) {
-    Write-Host "Installing PowerShell 7"
-    winget add -e --id Microsoft.PowerShell
-}
-
-$hasGit = Get-Command git -ErrorAction SilentlyContinue
-if (-not $hasGit) {
-    Write-Host "Installing Git"
-    winget add -e --id Git.Git
-}
-
 $hasChezmoi = Get-Command chezmoi -ErrorAction SilentlyContinue
 if (-not $hasChezmoi) {
     Write-Host "Installing chezmoi"
     winget add -e --id twpayne.chezmoi
 }
 
-
-chezmoi init https://github.com/trumully/dotfiles.git
+chezmoi init trumully -a
